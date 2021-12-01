3a9fa05084c540aa4d9b020f1009cc0e9eab616a
elenabissocoli03 <91657260+elenabissocoli03@users.noreply.github.com>
2021-12-01 18:00:44 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
+#include "lib.h"
+
+;char funzione(char a){
+  bool r;
+  char b;
+  if((65<=a && a<=90) || (97<=a && a<=122)){
+      r=true;
+  if(65<=a && a<=90) {
+      b=a+32;
+  }else{
+      b=a-32;
+  }
+  }else{
+      r=false;
+      b=1;
+  }
+  return b;
+}
+//
+// Created by Utente on 01/12/2021.
+//
 
